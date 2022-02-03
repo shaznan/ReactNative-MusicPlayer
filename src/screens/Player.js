cea1287d0c9107ed8ff715b95a782d8877dd1395
shaznan123 <shaznanfairozesally@gmail.com>
2022-02-03 20:56:57 +0530

    work on player screen - add player icon, text, and audio slider

--- src/screens/Player.js
+++ src/screens/Player.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Slider from "@react-native-community/slider";
 
+const { width } = Dimensions.get("window");
 export default function Player() {
   return (
     <View style={Styles.container}>
-      <Text>Player</Text>
-     </View>
+      <Text style={Styles.audioCount}>1 / 99</Text>
+      <View style={Styles.midBannerContainer}>
+        <MaterialCommunityIcons name="music-circle" size={300} color="black" />
+      </View>
+      <View style={Styles.audioPlayerContainer}>
+        <Text numberOfLines={1} style={Styles.audioTitle}>
+          Audio File Name
+        </Text>
+        <Slider
+          style={Styles.audioSlider}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+    </View>
   );
 }
 
 const Styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+  },
+  audioCount: {
+    padding: 20,
+    alignSelf: "flex-end",
+    opacity: 0.5,
+  },
+  midBannerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  audioTitle: {
+    fontSize: 15,
+    padding: 15,
+    alignSelf: "center",
+  },
+  audioSlider: {
+    height: 40,
+    width: (width * 90) / 100,
+    alignSelf: "center",
+  },
+});
